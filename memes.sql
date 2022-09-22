7ccfc289628301b215d39d1c0cc6d58ff330c567
Pazzurro <legaldamian133@gmail.com>
2022-09-22 14:17:00 +0200

    taq

--- memes.sql
+++ memes.sql
@@ -11,3 +11,10 @@ INSERT INTO users ("user_name", "password", "email", "is_admin") VALUES
 (
     "User2", MD5("Q@wertyuiop"), user2@wp.pl, 0,
 );
+
+
+SELECT m.id, m.file, m.title, m.likes, m.updated_at AS "date", u1.name AS "autor", u2.name AS "admin" 
+FROM memes m
+JOIN user u1 ON user_id = u1.id 
+JOIN user u2 ON admin_id = u2.id;
+WHERE m.deleted_at IS NULL OFFSET 0 LIMIT 10;
