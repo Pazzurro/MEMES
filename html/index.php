17101a30f903e1e4d2cd6a3cd573d87f04bf464a
Pazzurro <legaldamian133@gmail.com>
2022-10-05 14:26:23 +0200

    już prawie działa strona

--- html/index.php
+++ html/index.php
@@ -9,7 +9,7 @@
     $page = 1;
     $limit = 10;
     $offset = ($page * $limit) - $limit;
-    $q ="SELECT `file`, `likes`, `title`, `created_at` FROM memes limit $limit offset $offset ";     
+    $sql ="SELECT `file`, `likes`, `title`, `created_at` FROM memes limit $limit offset $offset ";     
 ?>
 
 
@@ -18,13 +18,55 @@
     <head>
         
         <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="style.css">
         <title>Memy</title>
-        <link rel="stylesheet" type="text/css" href="styl.css">
+        
         
     </head>
-    <body class="body">
+    <body>
+        
+        <div class="header">
+            <h2>MEMY</h2>
+        </div>
+        
+        <div>
+            
+            <div class="menu">
+                <h3>MENU</h3>
+                
+                <p class="menuButton">jakaś opcja</p>
+                <p class="menuButton">jakaś opcja</p>
+                <p class="menuButton">jakaś opcja</p>
+                <p class="menuButton">jakaś opcja</p>
+                <p class="menuButton">jakaś opcja</p>
+            </div>
+            
+            <div class="memes">
+                
+                <?php
+                    if($result = $db->query($sql))
+                    {
+                        while($row = $result->fetch_array())
+                        {
+                            echo '
+                                <div class="memeImage">
+                                    <img src=meme_Images/"' .$row["title"]. '".png"
+                                </div>
+                            ';
+                        }
+                    }
+                ?>
+                
+            </div>
+            
+        </div>
+        
+        
         
         <!-- trza zrobić php tutej, ale nie działa css -->
+        
+        
+        
         
     </body>
 </html>
